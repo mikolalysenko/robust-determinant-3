d7ef02a8b29d5b2de283c51ba8c825d3f6d9eee8
Mikola Lysenko <mikolalysenko@gmail.com>
2013-09-19 01:47:50 -0500

    tabs -> spaces

--- det3.js
+++ det3.js
@@ -7,8 +7,8 @@ var det2 = require("robust-determinant-2")
 module.exports = robustDeterminant3
 
 function robustDeterminant3(m) {
-	var A = robustScale(det2([[m[1][1], m[1][2]], [m[2][1], m[2][2]]]), m[0][0])
-	var B = robustScale(det2([[m[1][2], m[1][0]], [m[2][2], m[2][0]]]), m[0][1])
-	var C = robustScale(det2([[m[1][0], m[1][1]], [m[1][0], m[1][1]]]), m[0][2])
-	return robustSum(robustSum(A, B), C)
+  var A = robustScale(det2([[m[1][1], m[1][2]], [m[2][1], m[2][2]]]), m[0][0])
+  var B = robustScale(det2([[m[1][2], m[1][0]], [m[2][2], m[2][0]]]), m[0][1])
+  var C = robustScale(det2([[m[1][0], m[1][1]], [m[1][0], m[1][1]]]), m[0][2])
+  return robustSum(robustSum(A, B), C)
 }
