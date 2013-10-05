340677a2de45f39ff57f7c27eaad632c59871300
Mikola Lysenko <mikolalysenko@gmail.com>
2013-10-05 11:37:40 -0500

    more spacing fixes

--- det3.js
+++ det3.js
@@ -8,11 +8,11 @@ module.exports = robustDeterminant3
 
 function robustDeterminant3(m) {
   var A = robustScale(det2([[m[1][1], m[1][2]], 
-							[m[2][1], m[2][2]]]), m[0][0])
+                            [m[2][1], m[2][2]]]), m[0][0])
   var B = robustScale(det2([[m[1][0], m[1][2]], 
-							[m[2][0], m[2][2]]]), -m[0][1])
+                            [m[2][0], m[2][2]]]), -m[0][1])
   var C = robustScale(det2([[m[1][0], m[1][1]], 
-							[m[2][0], m[2][1]]]), m[0][2])
-							
+                            [m[2][0], m[2][1]]]), m[0][2])
+  						
   return robustSum(robustSum(A, B), C)
-}
+  }
