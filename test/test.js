a804b7678fb9846b908c350c24fb7db2ad1b67a0
Mikola Lysenko <mikolalysenko@gmail.com>
2013-10-05 11:33:45 -0500

    added test case

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 "use strict"
 
+var glm = require("gl-matrix")
 var det3 = require("../det3.js")
 
 require("tape")(function(t) {
@@ -7,6 +8,14 @@ require("tape")(function(t) {
 	t.same(det3([[1, 0, 0],
 							 [0, 1, 0],
 							 [0, 0, 1]]), [1])
+
+  for(var i=0; i<100; ++i) {
+    var r = new Array(3)
+    for(var j=0; j<3; ++j) {
+      r[j] = [ (10*Math.random())|0, (10*Math.random())|0, (10*Math.random())|0 ]
+    }
+    t.same(det3(r), [glm.mat3.determinant(r[0].concat(r[1]).concat(r[2]))])
+  }
 
 	t.end()
 })
